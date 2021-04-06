52028f2300b5a7f1ae9d8a5fbf6227325143c263
elkadiri.mohamed1815351@gmail.com <Mohamed13022000>
2021-04-07 00:40:30 +0200

    login form

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,25 +1,26 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
-import java.sql.Connection;
 
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setResizable(false);
+        primaryStage.setFullScreen(false);
+        Parent root = FXMLLoader.load(getClass().getResource("LogInForm.fxml"));
+        LogInController ctr = new LogInController();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        Scene scene = new Scene(root,1000,600);
+        scene.getStylesheets().add("sample/logInForm.css");
+        primaryStage.setScene(scene);
         primaryStage.show();
-
+    }
 
 
     public static void main(String[] args) {
