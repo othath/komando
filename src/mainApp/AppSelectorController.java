d88cece4abc67b48b1006ea1fa4f9ec64bd71e2e
Othmane <Othmane@OTHA-TH>
2021-04-29 21:59:30 +0000

    dashBoard interface

--- src/mainApp/AppSelectorController.java
+++ src/mainApp/AppSelectorController.java
@@ -9,14 +9,26 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import mainApp.client.LogInController;
 
+import java.io.IOException;
+
 public class AppSelectorController {
     @FXML private Button clientBtn;
+    @FXML private Button adminBtn;
 
     public void clientScene(ActionEvent Action) throws Exception{
+
         Stage stage = (Stage) clientBtn.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("client/res/logInForm.fxml"));
         Scene scene = new Scene(root, 1000,600);
         scene.getStylesheets().add(LogInController.class.getResource("res/LogInForm.css").toExternalForm());
         stage.setScene(scene);
+    }
+    public void adminScene(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("res/dashBoardInterface.fxml"));
+        Scene scene=new Scene(root, 800, 450);
+        scene.getStylesheets().add(getClass().getResource("res/dashboardInterface.css").toExternalForm());
+        Stage thisStage = (Stage) adminBtn.getScene().getWindow();
+        thisStage.setScene(scene);
+        //thisStage.show();
     }
 }
